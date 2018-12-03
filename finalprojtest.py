8273e0a8729a44190adf9077f1bce5882ed84bc4
cos <copa2705@colorado.edu>
2018-12-03 14:56:24 -0700

    Update finalprojtest.py

--- finalprojtest.py
+++ finalprojtest.py
@@ -46,29 +46,68 @@ def countBinner(reader):
         if row[6] > .5 and row[6] < .6:
           print "rows counted second level: " + str(counter)
           if row[18] > .3 and row[17] < .4:
-            #favoriteMatrix[6][0] = favoriteMatrix[6][0] + 1
+            favoriteMatrix[6][0] = favoriteMatrix[6][0] + 1
           if row[18] > .39 and row[17] < .5:
-            #favoriteMatrix[5][0] = favoriteMatrix[5][0] + 1
+            favoriteMatrix[5][0] = favoriteMatrix[5][0] + 1
           if row[18] > .49 and row[17] < .6:
-            #favoriteMatrix[4][0] = favoriteMatrix[4][0] + 1
+            favoriteMatrix[4][0] = favoriteMatrix[4][0] + 1
           if row[18] > .59 and row[17] < .7:
-            #favoriteMatrix[3][0] = favoriteMatrix[3][0] + 1
+            favoriteMatrix[3][0] = favoriteMatrix[3][0] + 1
           if row[18] > .69 and row[17] < .8:
-            #favoriteMatrix[2][0] = favoriteMatrix[2][0] + 1
+            favoriteMatrix[2][0] = favoriteMatrix[2][0] + 1
           if row[18] > .79 and row[17] < .9:
-            #favoriteMatrix[1][0] = favoriteMatrix[1][0] + 1 
+            favoriteMatrix[1][0] = favoriteMatrix[1][0] + 1 
           if row[18] > .89 and row[17] < 1:
-            #favoriteMatrix[0][0] = favoriteMatrix[0][0] + 1   
+            favoriteMatrix[0][0] = favoriteMatrix[0][0] + 1   
   print favoriteMatrix
           
       #away
       #if row[7] > .5:
         
-                 
+homeFiveThirtyEightProb = {}
+homeImpliedProb = {}
+
+
+def edgeDictionary():
+  print "hit"
+  counter = 0
+  for row in islice(reader, 1, None):
+    uniqueKey = row[0]
+    homeFiveProb = row[6]
+    homeImpliedProbability = row[17]
+    homeFiveThirtyEightProb[uniqueKey] = homeFiveProb
+    homeImpliedProb[uniqueKey] = homeImpliedProbability
+
+
+    
+    
+    
+    
+def edgeCalculator(dict1, dict2):
+  homeEdge = {}
+  print "hit2"
+  counter = 0
+  for item in dict1:
+    fiveThirtyProb = dict1[item]
+    impliedProb = dict2[item]
+    homeEdge[item] = round(float(fiveThirtyProb) - float(impliedProb),2)
+  return homeEdge
+  
+def edgeBinner(dict1):
+  edgeBins = {}
+  for item in homeEdge:
+    initialFloor = -.29
+    if homeEdge[item] >= initialFloor and < initialFloor + .01:
+      edgeBins
+      
+    
+                
 
 def setup():
   #implied()
-  countBinner(reader)
+  #countBinner(reader)
+  edgeDictionary()
+  edgeCalculator(homeFiveThirtyEightProb, homeImpliedProb)
     
 
   
